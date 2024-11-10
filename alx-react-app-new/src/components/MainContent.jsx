8a231a19899a866635dcab326c138cb91839bd4c
TlhogiM2 <tlhogi.tquet@gmail.com>
2024-11-10 12:11:15 +0200

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,1 +1,10 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', color: 'darkgreen', fontSize: '18px', textAlign: 'center' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
 
